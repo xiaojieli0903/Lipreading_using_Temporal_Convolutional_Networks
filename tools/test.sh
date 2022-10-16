7f7a3e4026caa980fce61b6fcef2445bdacc77ff
lixiaojie <xiaojieli@buaa.edu.cn>
2022-10-16 14:52:28 +0800

    update local

--- tools/test.sh
+++ tools/test.sh
@@ -1,6 +1,6 @@
 MODEL_JSON_PATH=$1
-ANNONATION_DIRECTORY=/home/gao2/disk/datasets/lrw/lipread_mp4/
-MOUTH_ROIS_DIRECTORY=/home/gao2/disk/datasets/lrw/visual_data
+ANNONATION_DIRECTORY=./datasets/lipread_mp4/
+MOUTH_ROIS_DIRECTORY=./datasets/visual_data
 MODEL_PATH=$2
 CUDA_VISIBLE_DEVICES=1 python main.py --modality video \
                                       --config-path $MODEL_JSON_PATH \
